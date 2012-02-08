1ad394b2f1fde9d0ed4e33f41d83de70de3db646
Ricardo Lanziano <ricardo.lanziano@gmail.com>
2012-02-08 18:22:39 -0500

    [general] Helper para obtener todos los foros en las vistas

--- app/controllers/ApplicationController.js
+++ app/controllers/ApplicationController.js
@@ -1,3 +1,13 @@
 module.exports = require('matador').BaseController.extend(function () {
   this.viewFolder = ''
+  this.addBeforeFilter(this.allForos)
 })
+  .methods({
+    allForos: function(callback) {
+      var self = this;
+      this.getModel('Foro').all(function(foros) {
+        self.response.foros = foros;
+        return callback(null)
+      })
+    }
+  });
