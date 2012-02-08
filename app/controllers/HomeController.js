7a5e267c9a4aadaa1dbf91963279b47c45eb43d2
Ricardo Lanziano <ricardo.lanziano@gmail.com>
2012-02-08 18:22:07 -0500

    [general] paso los foros al home controller

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,15 +1,15 @@
 module.exports = require(app.set('controllers') + '/ApplicationController').extend()
   .methods({
     index: function () {
-      this.render('index', {});
+      this.render('index', { foros: this.response.foros });
     },
     acerca: function() {
-      this.render('acerca', {});
+      this.render('acerca', { foros: this.response.foros });
     },
     ayuda: function() {
-      this.render('ayuda', {});
+      this.render('ayuda', { foros: this.response.foros });
     },
     tos: function() {
-      this.render('tos', {});
+      this.render('tos', { foros: this.response.foros });
     }
   })
