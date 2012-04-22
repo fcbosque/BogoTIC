3dc8383a509964db9fabd09da87539e019d9a910
cronopio <aristizabal.daniel@gmail.com>
2012-04-21 19:22:55 -0500

    [tests] pruebas iniciales para el modelo de Seguimiento

--- tests/SeguimientoModel-test.js
+++ tests/SeguimientoModel-test.js
@@ -12,15 +12,90 @@ app.configure(function () {
 
 var SeguimientoModel = require('../app/models/SeguimientoModel.js');
 
+var testSeguimiento = {
+  titulo: "Veeduria de Presupuesto",
+  descripcion: "Espacio para la participacion ciudadana usando las TIC's",
+  localidad: "Usme"
+};
+
 var testContext = {};
 
+function createForo (callback) {
+  var ForoModel = require('../app/models/ForoModel.js');
+
+  var testForo = {
+    nombre: "Rendicion Cuentas Presupuesto",
+    descripcion: "Foro para la rendicion de cuentas sobre el presupuesto publico.",
+    fecha: new Date(),
+    abierto: true
+  };
+
+  var nuevoForo = new ForoModel();
+
+  nuevoForo.create(testForo, callback);
+}
+
 /**
- * TERMINAR
- *
+ * Pruebas para la creacion de grupos de seguimiento
+ */
 vows.describe('app/models/SeguimientoModel').addBatch({
   "Modelo de Seguimiento": {
-    topic: new SeguimientoModel(),
-    ""
+    "revisando las clases": {
+      "y los modelos anexos": {
+        topic: new SeguimientoModel(),
+        "debe tener los 4 modelos anexos": function (clase) {
+          assert.isFunction(clase.Pregunta);
+          assert.isFunction(clase.Respuesta);
+          assert.isFunction(clase.Consulta);
+          assert.isFunction(clase.Informe);
+        },
+        "debe tener los constructores respectivos": function (clase) {
+          assert.equal(clase.Pregunta.modelName, 'Pregunta');
+          assert.equal(clase.Respuesta.modelName, 'Respuesta');
+          assert.equal(clase.Consulta.modelName, 'Consulta');
+          assert.equal(clase.Informe.modelName, 'Informe');
+        }
+      },
+      "y los metodos prometidos": {
+        topic: new SeguimientoModel(),
+        "debe tener create, show, remove, modify and all": function (clase) {
+          assert.isFunction(clase.create)
+          assert.isFunction(clase.show)
+          assert.isFunction(clase.remove)
+          assert.isFunction(clase.modify)
+          assert.isFunction(clase.all)
+        },
+        "debe tener los adicionales addPregunta y getPregunta": function (clase) {
+          assert.isFunction(clase.addPregunta);
+          assert.isFunction(clase.getPregunta);
+        }
+      }
+    },
+    "Instanciando": {
+      topic: new SeguimientoModel(),
+      "debe cargarse un modelo tipo seguimiento": function (seguimiento) {
+        assert.isFunction(seguimiento.DBModel);
+        assert.equal(seguimiento.DBModel.modelName, 'Seguimiento');
+      },
+      "debe tener las localidades": function (seguimiento) {
+        assert.isArray(seguimiento.localidades);
+      }
+    },
+    "Cargando": {
+      topic: function () {
+        var self = this;
+        createForo(function (err, foro) {
+          var nuevo = new SeguimientoModel();
+          nuevo.create(foro._id, testSeguimiento, self.callback);
+        });
+      },
+      "debe cargarse con los datos": function (err, grupo) {
+        assert.isNull(err);
+        assert.equal(grupo.titulo, testSeguimiento.titulo);
+        assert.equal(grupo.descripcion, testSeguimiento.descripcion);
+        assert.equal(grupo.localidad, testSeguimiento.localidad);
+        assert.ok(grupo.abierto);
+      }
+    }
   }
 }).export(module);
-*/
